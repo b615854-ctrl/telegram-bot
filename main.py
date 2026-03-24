16a63475ba13b87c28ac37012cafb1519ade8621
b615854-ctrl <b615854@gmail.com>
2026-03-24 15:25:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 from datetime import datetime, timedelta
 
 # ВСТАВЬТЕ ВАШ НОВЫЙ ТОКЕН
-TOKEN = '8675191350:AAFUqzm2sfl-u-kTfYTYBrLKpMJejPinV5M'
+TOKEN = '8675191350:AAEkUHBMxP7QxIlaQaT22KNM1usJtIDl-CI'
 ADMIN_GROUP_ID = -5172375951
 
 bot = telebot.TeleBot(TOKEN)
